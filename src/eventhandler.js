fcd2c9f1112807c40017c2c20ea2d624065ec7fe
fonsalir03 <weht03anon@gmail.com>
2025-12-22 22:57:19 -0600

    add property to hold all projects add property to hold tasks add feature that tracks task and its project when clicked add feature that allows edits to due date add feature that allows edits to priority

--- src/eventhandler.js
+++ src/eventhandler.js
@@ -4,11 +4,13 @@ export class EventHandler{
         this.dom = dom
         this.projectHandler = projectHandler;
         this.addListeners(this.dom)
+        this.projects = []
         this.currentProject = undefined
+        this.currentTask = {}
     }
 
-    findIndex(title, holder){
-        return holder.items.findIndex(item => item.name === title)
+    findIndexOf(title, holder){
+        return holder.findIndex(item => item.obj.name == title)
     }
 
     // callback functions
@@ -17,9 +19,11 @@ export class EventHandler{
         
         const projectTitle = formData.get("title");
         const projectDescription = formData.get("description");
-        const newProject = this.projectHandler.create(projectTitle, projectDescription);
+        const newProjectObj = this.projectHandler.create(projectTitle, projectDescription);
 
-        this.dom.renderProject(newProject);
+        const newProjectDOM = this.dom.renderProject(newProjectObj);
+        const newProject = {"dom": newProjectDOM, "obj" : newProjectObj};
+        this.projects.push(newProject);
 
     }
 
@@ -32,10 +36,11 @@ export class EventHandler{
         const taskPriority = formData.get("priority");
 
         const projectIndex = event.submitter.dataset.projectIndex;
-        const newTask = this.projectHandler.items[projectIndex].add(taskTitle, taskDescription, taskDueDate, taskPriority);
 
-        // render the task that was returned from project.add()
-        this.dom.renderTask(newTask, this.currentProject);
+        //add task obj to project obj
+        const newTaskObj = this.projectHandler.items[projectIndex].add(taskTitle, taskDescription, taskDueDate, taskPriority);
+        //render task in dom and add to project dom obj
+        this.dom.renderTask(newTaskObj, this.currentProject.dom);
 
     }
 
@@ -56,45 +61,122 @@ export class EventHandler{
 
     }
 
+    updateCurrentProject(projectTitle){
+        const projectIndex = this.findIndexOf(projectTitle, this.projects);
+        this.currentProject = this.projects[projectIndex];
+    }
+    updateCurrentTask(){
+        const taskIndex = this.currentProject.dom.currentTaskIndex;
+        this.currentTask = {"dom": this.currentProject.dom.domTasks[taskIndex], "obj": this.currentProject.obj.items[taskIndex]};
+    }
+
     addListeners(){
 
+        //debug button
+        const debugButton = document.querySelector("#view-projects");
+        debugButton.textContent = "debug"
+        debugButton.addEventListener("click", ()=> console.log(this.currentTask.dom.updatePriorityElem("high")))
+
+        document.body.addEventListener("change", (event)=> {
+            if (event.target.id == "new-priority-input"){
+
+                this.currentTask.obj.priority = event.target.value;
+                this.currentTask.dom.updatePriorityElem(this.currentTask.obj.priority);
+
+            }
+            if (event.target.id == "new-task-due-date"){
+
+                this.currentTask.obj.dueDate = event.target.value;
+                this.currentTask.dom.updateDueDate(this.currentTask.obj.dueDate);
+
+            }
+
+        })
+
         document.body.addEventListener("click", (event) => {
+
+            if (event.target.parentElement.id == "task-container" ){
+                const projectTitle = event.target.parentElement.parentElement.parentElement.firstChild.textContent;
+                this.updateCurrentProject(projectTitle);
+                this.updateCurrentTask();
+
+                //closes any open inputs
+                this.projects.forEach( (project)=> project.dom.domTasks.forEach( (task)=> task.checkForOpenInputs() ? task.closeOpenInputs() : ""));
+
+                if (event.target.nodeName == "BUTTON"){
+                    switch (event.target.id){
+                        case ("task-complete-toggle"):
+                            console.log("toggle the complete property");
+                            break;
+                        case ("task-details-toggle"):
+                            console.log("toggle the min and max methods");
+                            break;
+                        case ("task-delete-button"):
+                            console.log("delete this task");
+                            break;
+                    }
+                }
+                
+                switch (event.target.id){
+                    case ("task-title"):
+                        this.currentTask.dom.takeTitleInput();
+                        break;
+                    case("task-description"):
+                        console.log("user wants to edit the task description");
+                        break;
+                    case("task-due-date"):
+                        this.currentTask.dom.takeDueDateInput();
+                        break;
+                    case("task-priority"):
+                        this.currentTask.dom.takePriorityInput();
+                        break;
+                }
+                
+            } 
+            else if (event.target.parentElement.parentElement.id == "task-container"){
+                const projectTitle = event.target.parentElement.parentElement.parentElement.parentElement.firstChild.textContent
+                this.updateCurrentProject(projectTitle);
+                this.updateCurrentTask()
+                
+                if (event.target.id == "new-date-cancel"){
+                    this.currentTask.dom.removeDueDateInput();  
+                }
+                if (event.target.id == "new-priority-cancel"){
+                    this.currentTask.dom.removePriorityInput();
+                }
+            }
+
             // checks if a dialog is already rendered on the DOM
-            if (document.body.contains(this.dom.dialog) == false){
+            else if (document.body.contains(this.dom.dialog) == false){
                 // create project button click event
                 if (event.target.id == "create-project"){
                     this.dom.CreateForm("project");
                 }
                 // create task button click event
-                if (event.target.id == "project-add-task-button"){
+                else if (event.target.id == "project-add-task-button"){
 
                     const DOMProject = event.target.parentElement;
                     const DOMProjectTitle = DOMProject.children[0].textContent;
-                    const projectIndex = this.findIndex(DOMProjectTitle, this.projectHandler);
-                    
-                    //changing the selected project for the instance and dom variant
-                    this.projectHandler.select(projectIndex);
-                    this.currentProject = DOMProject;
+                    const projectIndex = this.findIndexOf(DOMProjectTitle, this.projects);
+                    this.currentProject = this.projects[projectIndex]
 
-                    this.dom.CreateForm("task", projectIndex);
+                    this.dom.CreateForm("task", projectIndex)
 
                 }
             }
-
-        });
-
-        //checks if form is being closed
-        document.body.addEventListener("click", (event) => {
-            if (event.target.id == "form-cancel-button"){
+            // checks if an form wants to be closed
+            else if (event.target.id == "form-cancel-button"){
                 this.dom.dialog.remove();
             }
+
         });
-        
+
         //checks if form is being submitted
         document.body.addEventListener("submit", (event)=> {
             if (event.target.id == "data-form"){
                 this.formHandler(event);
             }
         })
+
     }
 };

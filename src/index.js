834bbf448afe1b641f6c03bf78897940e715554f
fonsalir03 <194654788+fonsalir03@users.noreply.github.com>
2026-02-12 12:58:07 -0600

    fix bug where app would crash if local storage was empty

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import "./styles.css"
+
 import { Project } from "./project.js";
 
 import { DomHandler } from "./domHandler.js"
@@ -6,15 +8,13 @@ import { Handler } from "./handler.js"
 
 import { StorageHandler } from "./storageHandler.js";
 
-import "./styles.css";
-
 const domHandler = new DomHandler();
 const projectHandler = new Handler();
 const storage = new StorageHandler();
 const eventHandler = new EventHandler(domHandler, projectHandler, storage);
 
 //create the default project
-if (JSON.parse(localStorage.getItem("projects")).length == 0){
+if (storage.projects.length == 0){
     //generate default project
     const defaultProject = eventHandler.newProject("My Project", "This project contains tasks that are set to be completed");
     eventHandler.currentProject = defaultProject
@@ -22,3 +22,5 @@ if (JSON.parse(localStorage.getItem("projects")).length == 0){
     eventHandler.newTask(0, "walk my fish", "My fish has been skipping leg day recently, I need to make sure he is getting the exersize he needs!", "2025-11-21", "low", false)
     eventHandler.newTask(0, "finish the odin project", "I want to finish The Odin Project so I can make awesome applications and tools!", "2027-12-15", "high", false)
 }
+
+console.log("deployed from dist")
